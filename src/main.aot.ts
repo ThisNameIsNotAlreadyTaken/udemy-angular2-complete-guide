e003b05b7c4462baa6c0159cceb2c053a502be70
Vasya <eciseeyou@gmail.com>
2016-12-28 01:17:45 +0300

    main.aot added

---
+++ src/main.aot.ts
@@ -0,0 +1,6 @@
+import { platformBrowser }    from '@angular/platform-browser';
+import { MainModuleNgFactory } from '../aot/src/main.module.ngfactory';
+
+const platform = platformBrowser();
+
+platform.bootstrapModuleFactory(MainModuleNgFactory);
